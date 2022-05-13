b36b114917501c1be0f719fec6bcbfe0ca845b66
PiotrLonanS <vperunv@gmail.com>
2022-05-13 14:16:07 +0200

    1st glance on fightin
    Added unarmed animation health and damage

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -7,9 +7,15 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour , IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
+
         Transform target;
+        float timeSinceLastAttack = 0;
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
 
             if (!GetIsInRange())
@@ -19,7 +25,25 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
             }
+        }
+
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
+                //this will trigger that Hit() event.
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;              
+            }
+
+        }
+        // Animation Event
+        void Hit()
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
         }
 
         private bool GetIsInRange()
@@ -37,5 +61,6 @@ namespace RPG.Combat
         {
             target = null;
         }
+
     }
 }
